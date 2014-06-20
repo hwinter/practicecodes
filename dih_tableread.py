7614ba583321366ac1421bab85753de973739848
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-20 15:30:31 -0400

    attempt to fix peaks

--- dih_tableread.py
+++ dih_tableread.py
@@ -62,16 +62,16 @@ def dih_plotter(dirname,savename,numplot):
     for memberlist in plotlist:
         x = memberlist[0] #x coordinate data
         y = memberlist[1] #y coordinate data
-        peaklist =signal.find_peaks_cwt(y, np.arange(20,50))
+        peaklist =signal.find_peaks_cwt(y, np.arange(5,20))
         plt.plot(x,y,color = next(colors))
         for num in peaklist:
             plt.plot(x[num],y[num],'gD')#places markers on peaks
             plt.errorbar(x[num],y[num],xerr=0,yerr=0.15)
         peak = max(y)
         peaklist2 = [i for i, j in enumerate(y) if j==peak]
-    	for num in peaklist2:
-        	plt.plot(x[num],y[num],'rD')
-		plt.errorbar(x[num],y[num],xerr=0,yerr=0.15)
+#    	for num in peaklist2:
+#        	plt.plot(x[num],y[num],'rD')
+#		plt.errorbar(x[num],y[num],xerr=0,yerr=0.15)
 #finish up plot characteristics
 
     plt.title('Super Most Awesome Graph!')
