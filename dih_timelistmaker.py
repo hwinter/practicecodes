6282cc6b8c27ebb3b733fd5bbb5bbb26e0ae87f8
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-20 13:18:05 -0400

    created program to find indices of peak events

---
+++ dih_timelistmaker.py
@@ -0,0 +1,26 @@
+#Name: dih_timelistmaker.py
+#
+#Purpose: makes list of times (indices) of peak event
+#
+#
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('ps')
+import matplotlib.pyplot as plt 
+import pylab as P
+import glob
+import matplotlib.cm as cm
+from scipy import signal
+import dih_tableread as d
+
+def dih_timelistmaker(dirname):
+	indata = d.dih_tablereader(dirname)
+	timelist =[]
+	for memberlist in indata:
+		y = memberlist[1]
+		peak = max(y)
+		peaklist = [i for i, j in enumerate(y) if j==peak]
+		timelist.append(peaklist)
+	return timelist
+		
