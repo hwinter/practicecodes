633c6ba2a7342d0c70f0185fda186e0cb4131186
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-20 15:49:30 -0400

    edited error maker

---
+++ dih_errmaker.py
@@ -0,0 +1,33 @@
+#Name: dih_errmaker.py
+#
+#Purpose: makes rough estimate of error in peak time
+#
+#
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('ps')
+import matplotlib.pyplot as plt 
+import pylab as P
+import glob
+import matplotlib.cm as cm
+import dih_tableread as d
+import dih_maxlistmaker as dmax
+import dih_minlistmaker as dmin
+from matplotlib.mlab import find
+
+
+def dih_errmaker(dirname):
+	indata = d.dih_tablereader(dirname)
+	endlist =[]
+	for member in indata:
+		y = memberlist[1]
+		diff = abs(max(y)-min(y))
+		if diff > .8:
+			err = 6.3/7
+		else:
+			err = 6.3/3
+	endlist.append(err)
+	return endlist
+		
+		
