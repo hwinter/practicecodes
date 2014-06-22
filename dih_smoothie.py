404a168da81a19997c9dc90a1d8830fefa4e573b
Daniel Herman <dancloe42@gmail.com>
2014-06-22 16:20:56 -0400

    kaiser smoothing

---
+++ dih_smoothie.py
@@ -0,0 +1,62 @@
+#Name: dih_smooth
+#
+#Purpose: create kaiser smoothed version of input x with kaiser filter beta
+#
+#Inputs: array x and index beta
+#
+#
+import sys
+import numpy as np
+import matplotlib
+matplotlib.use('ps')
+import matplotlib.pyplot as plt 
+import pylab
+import glob
+import matplotlib.cm as cm
+from scipy import signal
+from dih_tableread import dih_filegrab
+from dih_tableread import dih_tablereader
+
+def dih_smooth(x,beta):
+    """ kaiser window smoothing """
+    window_len=11
+    # extending the data at beginning and at the end
+    # to apply the window at the borders
+    s = np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    w = np.kaiser(window_len,beta)
+    y = np.convolve(w/w.sum(),s,mode='valid')
+    return y[5:len(y)-5]
+#
+#
+#
+#Name:dih_plotter2
+#
+#Purpose:attempt at using kaiser smoothed data to find peaks and plot them
+#
+#See dih_tableread for more documentation
+
+def dih_plotter2(dirname,savename,numplot):
+    inlist = dih_tablereader(dirname)
+    plotlist = inlist[0:numplot]
+    colors = iter(cm.rainbow(np.linspace(0,1,len(plotlist)))) #creates color table
+    for memberlist in plotlist:
+        x = memberlist[0] #x coordinate data
+        y = memberlist[1] #y coordinate data
+        ysmooth = dih_smooth(y,16)
+        peaklist =signal.find_peaks_cwt(ysmooth, np.arange(1,30))
+        plt.plot(x,ysmooth,color = next(colors))
+        for num in peaklist:
+            plt.plot(x[num],ysmooth[num],'gD')#places markers on peaks
+        peak = max(ysmooth)
+        peaklist2 = [i for i, j in enumerate(ysmooth) if j == peak]
+        for num in peaklist2:
+            plt.plot(x[num],ysmooth[num],'rD')
+
+#finish up plot characteristics
+    plt.title('Super Most Awesome Graph!')
+    plt.ylabel('Flux')
+    plt.xlabel('Time')       
+    pylab.ylim([-5,5])
+    pylab.xlim([0,6.3])
+    plt.savefig(savename)#saves postscript file
+    return plotlist
