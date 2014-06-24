338eef1299c0bd01eaee63c4a1ec8444508324fd
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-24 17:00:19 -0400

    grabs fits files and makes map cube

---
+++ dih_sunplot.py
@@ -0,0 +1,14 @@
+import sunpy
+import sunpy.map
+import glob
+from sunpy.image.coalignment import mapcube_coalign_by_match_template
+
+def dih_sunplot(dirname):
+	filelist = glob.glob(dirname+"/*.fits")
+	maplist = []
+	for member in filelist:
+		my_map = sunpy.map.Map(member)
+		maplist.append(my_map)
+	mapcube = sunpy.map.Map(maplist,cube = True)
+	alignedcube = mapcube_coalign_by_match_template(mapcube)
+	
