ed1ab8c5582f6c10da6fe1740ae9ff6204e64ae4
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-25 12:48:13 -0400

    total flux getter

--- dih_suncurve.py
+++ dih_suncurve.py
@@ -2,8 +2,19 @@ import sunpy
 import sunpy.map
 import glob
 import numpy as np
-
-
+#Name:dih_suncurve
+#
+#Purpose: creates list of total flux in a series of AIA fits files
+#
+#Inputs:directory containing fits files (dirname)
+#
+#Outputs: list of total fluxes (curvelist)
+#
+#Example: dih_suncurve("../thataiadata")
+#
+#Written: 6/25/14 Dan Herman daniel.herman@cfa.harvard.edu
+#
+#
 def dih_suncurve(dirname):
 	filelist = glob.glob(dirname+"/*.fits")
 	curvelist = []
