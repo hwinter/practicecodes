3452f94414c10c45aced1c9c6cd12086d194bad7
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-25 14:29:05 -0400

    added channel to title

--- dih_lightcurve.py
+++ dih_lightcurve.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import dih_sunplot as times
 import dih_suncurve as values
 import dih_sundate as date
+import dih_sunchannel as channel
 #
 #Name:dih_lightcurve
 #
@@ -20,7 +21,7 @@ def dih_lightcurve(dirname,savename):
 	x = times.dih_sunplot_times(dirname)
 	y = values.dih_suncurve(dirname)
 	plt.plot(x,y,'b',linewidth = 2.0)
-	plt.title('Lightcurve at'+' '+date.dih_sunfirst(dirname),y=1.08)
+	plt.title('Lightcurve at'+' '+date.dih_sunfirst(dirname)+ ' '+ str(channel.dih_sunchannel(dirname))+'$\AA$',y=1.07)
 	plt.xlabel('Seconds Since'+' '+date.dih_sunfirst(dirname))
 	plt.ylabel('Arbitrary Flux Units')
 	plt.savefig(savename)
