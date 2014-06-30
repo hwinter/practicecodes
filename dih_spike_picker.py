123f7a2273fbd30d0be8408e5162596b2a4e51d4
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-06-30 10:30:11 -0400

    added new for loop to smooth inarray

--- dih_spike_picker.py
+++ dih_spike_picker.py
@@ -7,6 +7,21 @@ import pylab
 import glob
 import matplotlib.cm as cm
 from scipy import signal
+#
+#
+#Name: dih_spike_picker
+#
+#Purpose: finds spikes of data that are not flares and flattens them
+#
+#Inputs: array of lightcurve flux data (inarray)
+#
+#Outputs: new array of flux data with spikes removed
+#
+#Example: ynew = dih_spike_picker(y)
+#
+#Written: 6.30.14 Dan Herman daniel.herman@cfa.harvard.edu
+#
+#
 
 def dih_spike_picker(inarray):
     crest = max(inarray)
@@ -14,10 +29,12 @@ def dih_spike_picker(inarray):
     diff = crest - cave
     crestlist = [i for i, j in enumerate(inarray) if j == crest]
     for num in crestlist:
-        crestarray = inarray[num-2:num+2]
+        crestarray = inarray[num-2:num+3]
         subdiff = max(crestarray) - min(crestarray)
-        if subdiff > .75*diff:
-            inarray[num-2:num+2] = min(crestarray)
+        numlist = range(num-2,num+3)
+        if subdiff > .4*diff:
+            for member in numlist:
+            	inarray[member] = min(crestarray)
         else:
             print 'Not a false spike'
     return inarray
