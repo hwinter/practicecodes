aa378a8719e9581059a298f20e5801dacb26be01
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-07-01 10:53:26 -0400

    added new plotter to take advantage of second spike flattening program

--- dih_smoothsun.py
+++ dih_smoothsun.py
@@ -21,6 +21,8 @@ import dih_sunchannel as channel
 from scipy.signal import argrelextrema
 import dih_dir_finder as finder
 import pickle
+import dih_spike_picker as spike
+import dih_spike_picker2 as spike2
 
 #see dih_smoothie for documentation for dih_smooth module
 def dih_smooth(x,beta):
@@ -56,7 +58,7 @@ def dih_plotter3(dirname,savename,kaiser,boxcar,both):
     print fitslist
     outerdatalist = []
     for idx,dirpath in enumerate(fitslist):
-    	print "processing"+str(idx)
+    	print "processing "+str(idx)
     	innerdatalist = []
     	inlist = zip(*dih_lightcurvedata(dirpath))
     	plotlist = [list(row) for row in inlist]
@@ -77,13 +79,71 @@ def dih_plotter3(dirname,savename,kaiser,boxcar,both):
     	if both == 1:
     		ysmooth = dih_boxcar(dih_smooth(y,14))
     	#peaklist =signal.find_peaks_cwt(ysmooth, np.arange(1,10))#continuous wavelet transformation
-    	peaklist = argrelextrema(ysmooth, np.greater)
+    	yspikeless = spike.dih_spike_picker(ysmooth)
+    	peaklist = argrelextrema(yspikeless, np.greater)
     	plt.figure()
     	plt.plot(x,ysmooth,color = next(colors))
     	#for num in peaklist[0]:
     		#plt.plot(x[num],ysmooth[num],'gD')#places markers on peaks
-    	peak = max(ysmooth)
-    	peaklist2 = [i for i, j in enumerate(ysmooth) if j == peak]#places markers on absolute peaks
+    	peak = max(yspikeless)
+    	peaklist2 = [i for i, j in enumerate(yspikeless) if j == peak]#places markers on absolute peaks
+    	for num in peaklist2:
+    		plt.plot(x[num],ysmooth[num],'rD',linewidth =1.5)
+    		
+#finish up plot characteristics
+    	plt.plot(x,y,'b',linewidth = 1.0)
+    	plt.title('Lightcurve at'+' '+date.dih_sunfirst(dirpath)+ ' '+ str(channel.dih_sunchannel(dirpath))+'$\AA$',y=1.07)
+    	plt.xlabel('Seconds Since'+' '+date.dih_sunfirst(dirpath))
+    	plt.ylabel('Arbitrary Flux Units')
+    	plt.savefig(savename+str(idx)+'.ps')#saves postscript file
+    with open(savename+'.txt','wb') as ff:
+    	pickle.dump(outerdatalist,ff)
+    
+	return outerdatalist
+
+
+
+def dih_plotter4(dirname,savename,kaiser,boxcar,both):
+    fitslist = finder.dih_dir_finder(dirname)
+    print fitslist
+    outerdatalist = []
+    for idx,dirpath in enumerate(fitslist):
+    	print "processing "+str(idx)
+    	innerdatalist = []
+    	inlist = zip(*dih_lightcurvedata(dirpath))
+    	plotlist = [list(row) for row in inlist]
+    	colors = iter(cm.rainbow(np.linspace(0,1,len(plotlist)))) #creates color table
+    	x = plotlist[0] #x coordinate data
+    	innerdatalist.append(x)
+    	y = plotlist[1] #y coordinate data
+    	innerdatalist.append(y)
+    	#save each lightcurve's raw data into separate txt file
+    	with open(savename+str(idx)+'.txt','wb') as fff:
+    		pickle.dump(innerdatalist,fff)
+    	print innerdatalist
+    	outerdatalist.append(innerdatalist)#rough data to be sent to pickle dump
+    	if kaiser == 1:
+    		ysmooth = dih_smooth(y,14)#kaiser smoothing
+    	if boxcar == 1:
+    		ysmooth = dih_boxcar(y)#boxcar smoothing
+    	if both == 1:
+    		ysmooth = dih_boxcar(dih_smooth(y,14))
+    	yspikeless = spike2.dih_spike_picker2(ysmooth)
+    	peaklist =signal.find_peaks_cwt(yspikeless, np.arange(2,14))
+    	print peaklist
+    	crestlist = []
+    	cresttimelist = []
+    	for member in peaklist:
+    		crestlist.append(yspikeless[member])
+    		cresttimelist.append(x[member])
+    	crestlist = np.array(crestlist)
+    	uberlist = argrelextrema(crestlist, np.greater)
+    	plt.figure()
+    	plt.plot(x,ysmooth,color = next(colors))
+    	for member in uberlist[0]:
+    		plt.plot(cresttimelist[member],crestlist[member],'gD')#places markers on peaks
+    	peak = max(yspikeless)
+    	peaklist2 = [i for i, j in enumerate(yspikeless) if j == peak]#places markers on absolute peaks
     	for num in peaklist2:
     		plt.plot(x[num],ysmooth[num],'rD',linewidth =1.5)
     		
