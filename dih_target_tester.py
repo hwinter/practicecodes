258e38ed8181cd4eac0d9bde429e7067d4a497f1
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-07-02 16:58:46 -0400

    more testing

--- dih_target_tester.py
+++ dih_target_tester.py
@@ -1,55 +1,48 @@
 import pickle
 import dih_boxcar as d
-import dih_spike_picker2 as dd
+import dih_smoothsun as ddd
+import dih_spike_picker as dd
 import matplotlib.pyplot as plt
 from scipy import signal
 import numpy as np
 from scipy.signal import argrelextrema
+from dih_boxavg import dih_boxavg_recurs
 #
 #
 #Testing various signal processors on already created data
 
-def dih_target_tester(filename,num2):
+def dih_target_tester(filename,num1,num2):
 	list = pickle.load(open(filename+'.txt','rb'))
-	print list
-	target = list[1]
-	print target
-	domain = list[0]
-	target = d.dih_boxcar_recurs(target,23,4)
+	target = list[num1][1]
+	domain = list[num1][0]
+	target = dd.dih_spike_picker(target)
+	target = dd.dih_dip_picker(target)
+	window = 7
+	endrange = dih_boxavg_recurs(target,window,9)
 	targetlist = []
-	endrange = dd.dih_spike_picker2(target)
 	targetlist.append(domain)
 	targetlist.append(endrange)
 	#peaklist =signal.find_peaks_cwt(endrange, np.arange(2,12))
 	peaklist = argrelextrema(endrange,np.greater)
-	#sinklist = argrelextrema(endrange,np.less)
-	print peaklist
-	#print sinklist 
+	print peaklist 
 	plt.figure()
 	plt.plot(domain,endrange)
-	plt.plot(domain,list[1])
+	plt.plot(domain,list[num1][1])
 	crestlist = []
 	cresttimelist = []
-	#cavelist = []
-	#cavetimelist = []
+	extrapeaks = []
 	for member in peaklist[0]:
-		crestlist.append(endrange[member])
-		cresttimelist.append(domain[member])
-		plt.plot(domain[member],endrange[member],'yD')
-	#for member in sinklist[0]:
-		#cavelist.append(endrange[member])
-		#cavetimelist.append(domain[member])
+		if member < window or member > (len(endrange)-window):
+			extrapeaks.append(member)
+			
+			continue
+		else:
+			crestlist.append(endrange[member])
+			cresttimelist.append(domain[member])
+			plt.plot(domain[member],endrange[member],'yD')
+			continue	
 	print crestlist
-	#print cavelist
 	crestlist = np.array(crestlist)
-	#cavelist = np.array(cavelist)
 	uberlist = argrelextrema(crestlist, np.greater)
-	#suberlist = argrelextrema(cavelist,np.less)
-	print 'peaks'
-	print uberlist
-	#print 'sinks'
-	#print suberlist
-	#for member in uberlist[0]:
-		#plt.plot(cresttimelist[member],crestlist[member],'gD')#places markers on peaks
 	plt.savefig('test'+str(num2)+'.ps')
 	return targetlist
