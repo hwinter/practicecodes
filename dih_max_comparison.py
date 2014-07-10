f8b924622bc137943834ca6b91d7f7d3922874cb
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-07-10 17:49:23 -0400

    completed dummy code for 171 comparison and simplified max comparison

--- dih_max_comparison.py
+++ dih_max_comparison.py
@@ -26,6 +26,7 @@ from datetime import datetime
 from datetime import timedelta
 import simplejson
 import os.path
+import itertools
 #Name: dih_max_comparison
 #
 #Purpose: finds absolute peaks in different channels that correspond to each other temporally
@@ -44,54 +45,103 @@ import os.path
 
 def dih_max_comparison(dirname,filename):
 	fitslist = finder.dih_dir_finder(dirname)[0]#source ivo files to count over
+	ivolist = finder.dih_dir_finder(dirname)[1]
 	peaklist = []
 	sharedlist = []
 	for idx,member in enumerate(fitslist):
-		data = simplejson.load(open(filename+'_human_meta'+str(idx)+'.txt','rb'))
-		for member in data[4]:
-			timeval = datetime.strptime(member,'%Y/%m/%d %H:%M:%S.%f')
-			channel = data[1]
-			peaklist.append([timeval,channel])
-	cusp = datetime.timedelta(seconds = 180)
+		if os.path.isfile(filename+'_human_meta'+str(idx)+'.txt') == False:
+			continue
+		else:
+			data = simplejson.load(open(filename+'_human_meta'+str(idx)+'.txt','rb'))
+			for member in data[4]:#create manageable peak data sets
+				timeval = datetime.strptime(member,'%Y/%m/%d %H:%M:%S.%f')
+				channel = data[1]
+				values = [timeval,channel,ivolist[idx]]
+				peaklist.append(values)
+	cusp = timedelta(seconds = 180)#set temporal relatedness factor
 	for idx,member in enumerate(peaklist):
-		poppedlist = list.pop([idx])
 		subsharedlist = []
 		subsharedlist.append(member)
-		for guy in poppedlist:
+		separate_list = peaklist[0:idx] + peaklist[idx+1:len(peaklist)]
+		for guy in separate_list:#populate subsharedlist with closely related peaks
 			if guy[0]< member[0]+cusp and guy[0]>member[0]-cusp and guy[1] != member[1]:
 				subsharedlist.append(guy)
 				continue
 			else:
 				continue
-		subsharedlist = list(set(subsharedlist))
 		sharedlist.append(subsharedlist)
-	np.savetxt(filename+'sharedpeaks.txt',np.column_stack((subsharedlist)),header = 'List of shared peaks created on '+time.strftime("%c"))
+	for member in sharedlist:
+		for item in member:
+			item[0] = str(item[0])
+	with open(filename+'_pickled_timerel_peaks.txt','wb') as fff:
+		pickle.dump(sharedlist,fff)
+	savefile = open(filename+'_timerel_peaks.txt','wb')
+	simplejson.dump(sharedlist,savefile)
+	savefile.close()
 	return sharedlist
 		
 #
 #
 #Name: dih_171_comparison
 #
+#Purpose: sorts list of peaks into groups based upon temporal and spatial relatedness to 171 peaks
+#
+#inputs: Overall directory containing ivo files containing fits files to be analyzed (dirname)
+#savename used when originally creating sunpy maps (filename1)
+#savename used to create metadata tex files from Trae (filename2)
+#
+#Outputs: sharedlist containing lists of related peaks
+#
+#Example: list = dih_171_comparison('/Volumes/Scratch/Users/dherman/data_whoa','sunnywhoa','traetext.txt')
+#
+#Written: 7/10/14 Dan Herman daniel.herman@cfa.harvard.edu
+#
+#
 def dih_171_comparison(dirname,filename1,filename2):
 	fitslist = finder.dih_dir_finder(dirname)[0]
-    all_peaks = []
-    for idx,member in enumerate(fitslist):
-        if os.path.is_file(filename1+_'human_meta'+str(idx)+'.txt') == False:
-            continue
-        else:
-            data = simplejson.load(open(filename+'_human_meta'+str(idx)+'.txt','rb'))
-            #code will need to be changed once true format of text file is known
-            file = open('centerfilename')
-            lines = file.readlines()
-            center = lines[3]
-            for member in data[4]:
-                timeval = datetime.strptime(member,'%Y/%m/%d %H:%M:%S.%f')
-                channel = data[1]
-                all_peaks.append([timeval,channel,center])
-    list171 = []
-    for peak in all_peaks:
-        if peak[1] == 171:
-            list171.append(peak)
+	all_peaks = []
+	for idx,member in enumerate(fitslist):
+		if os.path.isfile(filename1 + '_human_meta' + str(idx) + '.txt') == False:
+			continue
+		else:
+			data = simplejson.load(open(filename + '_human_meta' + str(idx) + '.txt','rb'))
+			if data[7] == 'flag':
+				continue
+			#code will need to be changed once true format of text file is known
+			file = open(filename2)
+			lines = file.readlines()
+			center = lines[3]
+			for member in data[4]:
+				timeval = datetime.strptime(member,'%Y/%m/%d %H:%M:%S.%f')
+				channel = data[1]
+				all_peaks.append([timeval,channel,center])
+	list171 = [j for j, j in enumerate(all_peaks) if j[1] == 171]
+	other_list = [j for j, j in enumerate(all_peaks) if j[1] != 171]
+	t_cusp = timedelta(seconds = 180)
+	s_cusp = 3.0
+	sharedlist = []
+	for idx,peak in enumerate(list171):
+		subsharedlist = [peak]
+		for member in other_list:
+			if peak[0]<member[0]+t_cusp and peak[0]>member[0]-t_cusp and peak[2][0]<member[2][0]+s_cusp and peak[2][0]>member[2][0]-s_cusp and peak[1][0]<member[1][0]+s_cusp and peak[1][0]>member[1][0]-s_cusp:    			
+				subsharedlist.append(member)
+				continue
+			else:
+				continue
+		subsharedlist = list(set(subsharedlist))
+		sharedlist.append(subsharedlist)
+	for member in sharedlist:
+		for item in member:
+			item[0] = str(item[0])
+	with open(filename1+'_pickled_related_peaks.txt','wb') as fff:
+		pickle.dump(sharedlist,fff)
+	savefile = open(filename1+'_related_peaks.txt','wb')
+	simplejson.dump(sharedlist,savefile)
+	savefile.close()
+	return sharedlist
+    		
+    		
+
             
             
         
