be5e97035ec9e243eb09e83b78c5bc570288b842
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-07-29 16:43:05 -0400

    added module for cropped plotter

--- dih_hist_171_peaks.py
+++ dih_hist_171_peaks.py
@@ -145,6 +145,80 @@ def dih_hist_events(filename1,savename,channel1,channel2):
 		
 	return final 		
 			
-				
+#
+#Name: dih_hist_crop_events
+#
+#Purpose: flexibly create histogram for two channels of choice displaying separation between peaks in those channels (for events processed with dih_sun_cropped_plotter)
+#
+#Inputs: filename1 -> metadata to analyze, savename -> save name for histogram, channel1 -> target channel, channel2 -> testing channel
+#
+#Outputs: histogram and final data [time differences,source ivo for channel1, source ivo for channel2]
+#
+#Example: gah = dih_hist_events('metadata.txt','hist.ps',193,131)
+#
+#Written: 7/29/14 Dan Herman daniel.herman@cfa.harvard.edu
+#
+#
+def dih_hist_crop_events(filename1,savename,channel1,channel2):
+	#get event sets
+	uber_list = dih_event_crop_select(filename1)
+	time_diff_channel = []
+	target_ivo = []
+	target_time_list = []
+	other_time_list = []
+	other_ivo = []
+	outlier_list = []
+	for member in uber_list:
+		for guy in member:
+			target = []
+			other = []
+			for dude in guy:
+				dude = list(dude)
+				if dude[1] == channel1:
+					target.append(dude)
+				if dude[1] == channel2:
+					other.append(dude)
+			#making sure lists are correct sizes
+			if len(target) > 1:
+				target = [target[0]]
+			elif len(target) == 0:
+				continue	
+			if len(other) > 1:
+				other = [other[0]]
+			elif len(other) == 0:
+				continue
+			target_time = datetime.strptime(target[0][0],'%Y/%m/%d %H:%M:%S.%f')
+			other_time = datetime.strptime(other[0][0],'%Y/%m/%d %H:%M:%S.%f')
+			delt = other_time-target_time
+			time_diff_channel.append(delt.total_seconds())
+			target_ivo.append(target[0][2])
+			other_ivo.append(other[0][2])
+			target_time_list.append(target[0][0])
+			other_time_list.append(other[0][0])
+			if abs(delt.total_seconds()) > 500:
+				outlier_data = (delt.total_seconds,target[0][2],other[0][2],target[0][0],other[0][2])
+				outlier_list.append(outlier_data)
+	final = [time_diff_channel,target_ivo,other_ivo,target_time_list,other_time_list]
+	final = list(set(zip(*final)))
+	final = zip(*final)
+	if len(time_diff_channel) == 0:
+		return [channel1,channel2,'no matches']
+	final.append(channel1)
+	final.append(channel2)
+	final.append(outlier_list)
+	P.figure()
+	print final
+	print 'here'
+	n, bins, patches = P.hist(list(final[0]),10, histtype = 'stepfilled')
+	final.append(tuple(n))
+	final.append(tuple(bins))
+	P.setp(patches, 'facecolor','b','alpha',0.75)
+	P.xlabel('Time Difference between '+str(channel1)+' and ' + str(channel2) + ' peak in seconds')
+	P.ylabel('Number of Peaks')
+	P.title('Histogram of ' + str(channel1) + ' & ' + str(channel2) + ' Separations')
+	P.savefig(savename)
+		
+	return final 		
+							
 				
 			
