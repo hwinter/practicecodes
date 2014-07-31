f1138084e0cea7f85e6a1d6fd725fb4b201e6278
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-07-31 17:16:51 -0400

    made return variables more informative, added docs

--- dih_sun_data_goes_plot.py
+++ dih_sun_data_goes_plot.py
@@ -42,16 +42,22 @@ from dih_run_idl_script import dih_run_idl_goes_script
 #
 #Purpose: plot lightcurve and find peaks for GOES lightcurve data
 #
-# 
-def dih_sun_data_goes_plot(datalist,start_time):
+#Inputs: datalist -> columns of raw goes data, start_time -> goes start time, savename -> over arching save string
+#
+#Outputs: metadata and smoothed data for each goes lightcurve
+#
+#Example: meta_and_smoothdata = dih_sun_data_goes_plot(columns,'05-1-2012 04:18:59.00','uber_string')
+#
+#Written:7/31/14 Dan Herman daniel.herman@cfa.harvard.edu
+#
+#
+def dih_sun_data_goes_plot(datalist,start_time,savename):
 	colors = iter(cm.rainbow(np.linspace(0,1,len(datalist[0])))) #creates color table
 	x = datalist[0] #x coordinate data
 	y = datalist[1] #y coordinate data
-	#time difference data
-	x = time_info_list[1]
 	ycopy = y
-	ysmooth = box.dih_boxavg_recurs(y,3,1)
-	window = 3	
+	window = 35
+	ysmooth = box.dih_boxavg_recurs(y,window,2)
 	peaklist = argrelextrema(ysmooth,np.greater)#relative max
 	peak = max(ysmooth[(window-1)/2:len(ysmooth)-(window-1)/2])#absolute max ignoring the very ends of the data set
 	maxpeaklist = [i for i, j in enumerate(ysmooth) if j == peak]
@@ -63,7 +69,7 @@ def dih_sun_data_goes_plot(datalist,start_time):
 		else:
 			plt.plot(x[member],ysmooth[member],'yD',markersize = 8)
 			#recreating peak times from time difference data
-			first_time = datetime.strptime(start_time,'%d-%m-%Y %H:%M:%S.%f)
+			first_time = datetime.strptime(start_time,'%d-%m-%Y %H:%M:%S.%f')
 			timediff = timedelta(seconds = x[member])
 			peaktime = first_time+timediff
 			relpeaktimelist.append(peaktime.strftime('%Y/%m/%d %H:%M:%S.%f'))
@@ -71,33 +77,35 @@ def dih_sun_data_goes_plot(datalist,start_time):
 	maxpeaktimelist = []
 	flagged_peaktimelist=[]
 	for member in maxpeaklist:
-		first_time = datetime.strptime(start_time, '%d-%m-%Y %H:%M:%S.%f)
+		first_time = datetime.strptime(start_time, '%d-%m-%Y %H:%M:%S.%f')
 		timediff = timedelta(seconds = x[member])
 		peaktime = first_time+timediff
-		if x[member] > 250 and x[member] < x[-1]-250:
+		if x[member] > 100 and x[member] < x[-1]-100:
 			maxpeaktimelist.append(peaktime.strftime('%Y/%m/%d %H:%M:%S.%f'))
 		else:
 			flagged_peaktimelist.append(peaktime.strftime('%Y/%m/%d %H:%M:%S.%f'))
-	real_peaklist = [j for j, j in enumerate(maxpeaklist) if x[j] > 250 and x[j] < x[-1]-250]
+	real_peaklist = [j for j, j in enumerate(maxpeaklist) if x[j] > 100 and x[j] < x[-1]-100]
 	print maxpeaklist
 	print real_peaklist
-	flagged_peaklist = [j for j, j in enumerate(maxpeaklist) if x[j] < 250 or x[j] > x[-1]-250]
+	flagged_peaklist = [j for j, j in enumerate(maxpeaklist) if x[j] < 100 or x[j] > x[-1]-100]
 	print flagged_peaklist
 	for member in real_peaklist:
 		plt.plot(x[member],ysmooth[member],'gD',markersize = 8)
 	for member in flagged_peaklist:
 		plt.plot(x[member],ysmooth[member],'rD',markersize = 8)
 	#creating chi-squared value
-	observed = np.array(ycopy)
-	expected = np.array(ysmooth)*np.sum(observed)
-	chi = chisquare(observed,expected)
+	#observed = np.array(ycopy)
+	#expected = np.array(ysmooth)*np.sum(observed)
+	#chi = chisquare(observed,expected)
+	fits_time = datetime.strptime(start_time,'%d-%m-%Y %H:%M:%S.%f')
+	start_time2 = datetime.strftime(fits_time,'%Y-%m-%dT%H:%M:%S.%f')
 	metadatalist = []
-	metadatalist.append(start_time)
+	metadatalist.append(start_time2)
 	metadatalist.append('GOES 1-8 Angstroms')
 	metadatalist.append('Full sun Goes image')
 	metadatalist.append(relpeaktimelist)
 	metadatalist.append(maxpeaktimelist)
-	metadatalist.append(ivo_list[num])
+	metadatalist.append('data from sdac archive')
 	metadatalist.append(flagged_peaktimelist)
 	smooth_range = max(ysmooth)-min(ysmooth)
 	if smooth_range > max(ysmooth)*.2:
@@ -112,29 +120,36 @@ def dih_sun_data_goes_plot(datalist,start_time):
 		#file_peakflag.close()
 	else:
 		metadatalist.append('no_peakflag')
-	fits_time = datetime.strptime(start_time,'%d-%m-%Y %H:%M:%S.%f)
 	fits_range = timedelta(seconds = x[-1])
 	fits_end = fits_time+fits_range
 	fits_end = datetime.strftime(fits_end,'%Y-%m-%dT%H:%M:%S.%f')
 	metadatalist.append(fits_end)	
 	plt.plot(x,y,'b',linewidth = 1.0)
 	plt.plot(x,ysmooth,'r',linewidth = 1.5)
-	plt.title('Lightcurve at' + ' ' + start_time + ' GOES 1-8 $\AA$',y=1.07)
-	plt.xlabel('Seconds Since' + ' ' + fits_date)
+	plt.title('Lightcurve at' + ' ' + start_time2 + ' for GOES 1-8 $\AA$',y=1.07)
+	plt.xlabel('Seconds Since' + ' ' + start_time2)
 	plt.ylabel('Arbitrary Flux Units')
-	plt.savefig('/data/george/dherman/sun_plots/' + newname + '_' + start_time + '_' + savename + str(num) + '.ps')#saves postscript file
-	return metadatalist
+	plt.savefig('/data/george/dherman/sun_plots/' + start_time2 + '_' + savename + '_GOEScurve.ps')#saves postscript file
+	return [metadatalist,x,ysmooth]
 #
 #
 #
+#Name: dih_sun_recurs_goes_plot
 #
+#Purpose: recurses dih_sun_data_goes_plot for all files in file_131 containing data for 131 channel
 #
+#Inputs: file_131 -> text file containing data for 131 channel only, savename -> uber save string
 #
+#Outputs: uber_goes_metadatalist -> contains all paired 131/goes metadata
 #
+#Example: gah = dih_sun_recurs_goes_plot('all_131.txt','ubersave')
 #
-def dih_sun_recurs_goes_plot(131_file,savename):
-	data_file = open(131_file,'r')
-	meta_lines = data_files.readlines()
+#Written: 7/31/14 Dan Herman daniel.herman@cfa.harvard.edu
+#
+
+def dih_sun_recurs_goes_plot(file_131,savename):
+	data_file = open(file_131,'r')
+	meta_lines = data_file.readlines()
 	uber_goes_metadatalist = []
 	for member in meta_lines:
 		member = ast.literal_eval(member)
@@ -149,9 +164,10 @@ def dih_sun_recurs_goes_plot(131_file,savename):
 		with open(savename + '_goes_curve_' + start_string + '.txt','r') as f:
 			first_line = [f.readline()]
 			real_first_time = dih_create_goes_times(first_line)[0]
-		metadatalist = dih_sun_data_goes_plot(columns,real_first_time)
+		list_goes = dih_sun_data_goes_plot(columns,real_first_time,savename)
+		metadatalist = list_goes[0]
 		meta_file = open('/data/george/dherman/metadata/' + savename + '_all_human_meta_131_goes.txt','a')
-		simplejson.dump([member,metadatalist],meta_file)
+		meta_file.write(str([member,metadatalist,list_goes[1],list(list_goes[2])]))
 		meta_file.write('\n')
 		meta_file.close()
 		uber_goes_metadatalist.append(metadatalist)
