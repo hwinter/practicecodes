6a32ae3d5131529010d4d47efe961f8b7141f2e7
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-08-01 18:26:29 -0400

    subprocess killall idl

--- dih_run_idl_script.py
+++ dih_run_idl_script.py
@@ -1,11 +1,19 @@
 import pidly
 from dih_tableread import dih_filegrab2
 import ast
+import subprocess
 #
+#Name: dih_run_idl_goes_script
 #
+#Purpose: runs an idl program to extract goes data for a certain time range
 #
+#Inputs: start and end time strings in format 'day-month(abbreviation)-year hours:minutes:seconds.microseconds', save_name -> file name to be created by dih_get_goes.pro
 #
+#Outputs: columns of goes data in a list (2 columns in one list)
 #
+#Examples: columns = dih_run_idl_goes_script('01-May-2012 00:00:00.00','02-May-2012 00:00:00.00','goes_curve.txt')
+#
+#Written: 7/31/14 Dan Herman daniel.herman@cfa.harvard.edu
 #
 #
 
@@ -17,4 +25,6 @@ def dih_run_idl_goes_script(start_time,end_time,save_name):
 	#txt_lines = txt_file.readlines()
 	#txt_header = ast.literal_eval(txt_lines[0])
 	columns = dih_filegrab2(save_name)
+	idl.close()
+	subprocess.call(["killall", "idl"])
 	return zip(*columns)
