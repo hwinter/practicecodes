d94b1374a886a3b01356c04c56e874596417702a
Daniel Herman <daniel.herman@cfa.harvard.edu>
2014-08-14 16:55:01 -0400

    unused program

--- dih_basichist.py
+++ dih_basichist.py
@@ -13,6 +13,8 @@ import glob
 import matplotlib.cm as cm
 from scipy import signal
 import dih_tableread as d
+import matplotlib.mlab as mlab
+from scipy.stats import norm
 
 def dih_basichist(dirname,histname):
 	indata = d.dih_tablereader(dirname)
@@ -22,7 +24,10 @@ def dih_basichist(dirname,histname):
 		peak = max(y)
 		peaklist.append(peak)
 	P.figure()
-	n, bins, patches = P.hist(peaklist,12,normed = 1, histtype = "stepfilled")
+	n, bins, patches = P.hist(peaklist,12, histtype = "stepfilled")
+	(mu,sigma) = norm.fit(peaklist)
+	plotline = mlab.normpdf(bins,mu,sigma)
+	plt.plot(bins, plotline)
 	plt.xlabel('Max value of Random Data')
 	plt.ylabel('Fraction of Counts')
 	plt.title('Practice with Histograms')
